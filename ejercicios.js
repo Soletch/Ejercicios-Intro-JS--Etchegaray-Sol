2ac5b500144f9f8be5193f62305bd81a25565a78
Soletch <soletchegaray3@gmail.com>
2023-04-28 09:34:19 -0300

    first commit

---
+++ ejercicios.js
@@ -0,0 +1,46 @@
+//ejercicio 1
+
+let miNombre;
+
+miNombre = "Sol";
+
+//ejercicio 2
+
+let miApellido = "Etchegaray";
+
+//Ejercicio 3
+
+let miEdad = 19;
+
+//ejercicio 4 
+
+let miMascota = "Flor";
+
+//ejercicio 5
+
+let edadMascota = 10;
+
+//ejercicio 6
+
+console.log(miNombre);
+console.log(miApellido);
+console.log(miEdad);
+console.log(miMascota);
+console.log(edadMascota);
+
+
+//ejercicio 7
+
+//const nombreCompleto =  ${miNombre} ${miApellido}´ ;
+
+//console.log(nombreCompleto);
+
+//ejercicio 8
+
+let textoPresentacion;
+
+console.log("Me llamo " + miNombre + " " + miApellido + " y tengo " + miEdad + ". Tengo una perra que se llama " + miMascota + " que tiene " + edadMascota);
+
+//ejercicio 9
+
+let sumaEdades 
